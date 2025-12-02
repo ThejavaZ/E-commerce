e746fe64286a0c20d5ac8c8c53aace474bf23e3e
JavierSG <jsdash10000@gmail.com>
2025-12-01 22:29:01 -0700

    Se empezo los controladores y actualizacion a usar VueJS

--- ecommerce/resources/views/layouts/main.blade.php
+++ ecommerce/resources/views/layouts/main.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>E-commerce</title>
-    @vite(['resources/css/app.css', 'resources/js/index.tsx'])
+    @vite(['resources/css/app.css', 'resources/js/main.js'])
 </head>
 <body>
     <div id="app"></div>
--- ecommerce/vite.config.js
+++ ecommerce/vite.config.js
@@ -1,31 +1,15 @@
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ["resources/css/app.css", "resources/js/index.tsx"],
+            input: ["resources/css/app.css", "resources/js/main.js"],
             refresh: true,
         }),
-
-        react({
-            jsxRuntime: "automatic",
-        }),
         tailwindcss(),
+        vue(),
     ],
-    resolve: {
-        alias: {
-            "@": "/resources/js/",
-        },
-    },
-    server: {
-        hmr: {
-            host: "localhost",
-        },
-    },
-    optimizeDeps: {
-        include: ["react", "react-dom"],
-    },
 });
